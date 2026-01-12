6b9a61e664abba0dc9680a50e51efccb9047d15c
goyalyash2515-png <goyalyash2515@gmail.com>
2026-01-12 11:11:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,31 @@ function checkPassword() {
   if (pass === "wemeton15062025") {
     document.getElementById("passwordBox").style.display = "none";
     document.getElementById("mainContent").style.display = "block";
+    document.getElementById("song").play();
+    startCountdown();
   } else {
     alert("Wrong Password ❤️");
   }
+}
+
+function startCountdown() {
+  const birthday = new Date("January 17, 2026 00:00:00").getTime();
+
+  setInterval(() => {
+    const now = new Date().getTime();
+    const diff = birthday - now;
+
+    if (diff < 0) {
+      document.getElementById("countdown").innerHTML = "🎂 Happy Birthday Priyanshi 🎉";
+      return;
+    }
+
+    const d = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const h = Math.floor((diff / (1000 * 60 * 60)) % 24);
+    const m = Math.floor((diff / (1000 * 60)) % 60);
+    const s = Math.floor((diff / 1000) % 60);
+
+    document.getElementById("countdown").innerHTML =
+      ⏳ ${d} Days ${h} Hours ${m} Minutes ${s} Seconds;
+  }, 1000);
 }
